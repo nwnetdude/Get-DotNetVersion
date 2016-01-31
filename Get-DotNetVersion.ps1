454a0ee5fd9840f6d91c37bfc85cff1be5abead8
tim evans <netdude@gmail.com>
2016-01-31 10:20:06 -0800

    Add Test-Domain function

--- Get-DotNetVersion.ps1
+++ Get-DotNetVersion.ps1
@@ -1,7 +1,6 @@
-﻿function Get-DotNetVersion
-{
+﻿function Get-DotNetVersion {
     Clear-Host
-    Write-Host 'Installed .NET Framework Components' -ForegroundColor Yellow
+    Write-Host 'Installed .NET Framework Components' -ForegroundColor Green
     Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse |
     Get-ItemProperty -Name Version,Release -ErrorAction 0 |
     Where-Object { $_.PSChildName -match '^(?!S)\p{L}'} |
@@ -9,4 +8,24 @@
     Sort-Object -Property Version -Descending |
     Format-Table -AutoSize
 }
+
+function Test-Domain {
+[CmdletBinding()]
+Param (
+    [parameter(position=0,
+        Mandatory=$true,
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true)]
+    [string]$ComputerName='.'
+)
+Begin {}
+Process {
+    Write-Host 'Domain or Workgroup Membership' -ForegroundColor Green
+    Get-WmiObject -Class Win32_ComputerSystem -ComputerName $ComputerName | 
+        Select-Object -Property Name, Domain | Format-Table -AutoSize
+}
+End {}
+}
+
 Get-DotNetVersion
+Test-Domain -ComputerName 'localhost'
